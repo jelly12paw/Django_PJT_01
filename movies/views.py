090f520c12726ce456c76748772090a1bb5ea2fe
Hye <jelly12paw@gmail.com>
2022-09-30 11:31:00 +0900

    create new.html

--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Review
+from multiprocessing import context
 
 # Create your views here.
 def index(request):
@@ -6,3 +8,10 @@ def index(request):
 
 def new(request):
     return render(request, 'movies/new.html')
+
+def create(request):
+    title = request.GET.get('title_')
+    content = request.GET.get('content_')
+    Review.objects.create(title=title, content=content)
+
+    return redirect('movies:index')
