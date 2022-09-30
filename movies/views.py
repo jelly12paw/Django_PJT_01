2a04d3c559f28d2a67579ac93767de1f108a27ad
Yoonsik-Shin <sys25800994@gmail.com>
2022-09-30 13:34:32 +0900

    add edit page

--- movies/views.py
+++ movies/views.py
@@ -3,8 +3,11 @@ from .models import Review
 
 # Create your views here.
 def index(request):
-
-    return render(request, 'movies/index.html')
+    movies = Review.objects.all()
+    context = {
+        'movies': movies,
+    }
+    return render(request, 'movies/index.html', context)
 
 def new(request):
 
@@ -16,3 +19,12 @@ def create(request):
     Review.objects.create(title=title, content=content)
 
     return redirect('movies:index')
+
+def detail(request, pk):
+    movie_click = Review.objects.get(id=pk)
+
+    context = {
+        'title': movie_click.title,
+        'content': movie_click.content,
+    }
+    return render(request, 'movies/detail.html', context)
