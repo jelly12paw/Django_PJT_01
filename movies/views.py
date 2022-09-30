ef3d23dd63259378b07eb0915882be8bf5301347
Yoonsik-Shin <sys25800994@gmail.com>
2022-09-30 13:03:51 +0900

    중간정리

--- movies/views.py
+++ movies/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect
 from .models import Review
-from multiprocessing import context
 
 # Create your views here.
 def index(request):
+
     return render(request, 'movies/index.html')
 
 def new(request):
+
     return render(request, 'movies/new.html')
 
 def create(request):
