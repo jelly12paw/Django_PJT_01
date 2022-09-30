9fbb802b8bc077a1e2539548cf64cfa2b635603d
Yoonsik-Shin <sys25800994@gmail.com>
2022-09-30 10:48:11 +0900

    add index page

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'movies/index.html')
+
+def new(request):
+    return render(request, 'movies/new.html')
